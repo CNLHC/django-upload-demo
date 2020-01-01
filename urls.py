3285f498a4824066ff00be2886a914e847dfceb7
CNLHC <2563765697@qq.com>
2020-01-01 11:51:23 +0800

    demo

--- urls.py
+++ urls.py
@@ -5,5 +5,7 @@ from django.conf import settings
 
 
 
-urlpatterns = [ path('file/',FileManageView.as_view()) ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-print(urlpatterns)
+urlpatterns = [ path('file/',FileManageView.as_view())  ] 
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=static('/media/', document_root=settings.MEDIA_ROOT)
+
